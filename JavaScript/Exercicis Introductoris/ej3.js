dc16cf0369c63c8c9ae999e2e347b3a13ba3597f
Hugo Serra Ruiz <hugoserraruiz@gmail.com>
2023-10-18 08:55:46 +0200

    Terminado ejercicio 3

---
+++ JavaScript/Exercicis Introductoris/ej3.js
@@ -0,0 +1,15 @@
+//DECLARACION DE VARIABLES
+var nombre = "Hugo";
+var apellido = "Serra";
+var edad = 20;
+var nacimiento = 2003;
+
+//MENSAJES ALERT
+//Alert con comillas simples
+alert("'Frase con comillas simples'");
+//Alert nombre y apellido separados con salto de linea
+alert(nombre+"\n"+apellido);
+//Alert suma de variables de año y edad
+alert(edad+nacimiento);
+//Alert suma de todas las variables
+alert(nombre+apellido+edad+nacimiento);
