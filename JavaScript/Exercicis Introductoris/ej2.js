d624d28ce63bbdc6bff23f3e84dd70f371d92346
Hugo Serra Ruiz <hugoserraruiz@gmail.com>
2023-10-18 08:47:24 +0200

    Acabado el ejercicio 2

---
+++ JavaScript/Exercicis Introductoris/ej2.js
@@ -0,0 +1,13 @@
+//DECLARACION DE VARIABLES
+var entero = 1357;
+var decimal = 135.7;
+var cientifico = 135e7;
+var octal = 0o1357;
+var hexa = 0x1357;
+
+//MENSAJES AL USUARIO CON LAS VARIABLES CREADAS
+alert("Numero entero: "+entero);
+alert("Numero decimal: "+decimal);
+alert("Numero cientifico: "+cientifico);
+alert("Numero octal: "+octal);
+alert("Numero hexadecimal: "+hexa);
