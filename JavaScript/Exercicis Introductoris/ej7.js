5c795c65c815167f38cb2a6910ae666792988810
Hugo Serra Ruiz <hugoserraruiz@gmail.com>
2023-10-30 08:32:06 +0100

    Terminado el ej7

---
+++ JavaScript/Exercicis Introductoris/ej7.js
@@ -0,0 +1,9 @@
+num1 = prompt("Dime un numero");
+num2 = prompt("Dime otro numero");
+
+suma = parseInt(num1) + parseInt(num2);
+resta = parseInt(num1) - parseInt(num2);
+mult = parseInt(num1) * parseInt(num2);
+division = parseInt(num1) / parseInt(num2);
+
+alert ("suma: " + suma + "\nresta: " + resta + "\nmultiplicacion: " + mult + "\ndivision: "+ division);
